a9ab8ec434742e7bc78675bb50418ffb2e5f5fcd
southplus <jasminelu7777@gmail.com>
2019-08-25 21:31:02 +0800

    if wants to change

--- PullRequest.js
+++ PullRequest.js
@@ -1,3 +1,5 @@
 //add new file
 
 // feature one: i‘will change one line
+
+//if another person wants to change this pull request too, you should pull this branch into your pc, and work on this branch
